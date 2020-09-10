1a4db94c07227dabdfa0875eb8093634a6f6cce9
Ben <ben.conway@xdesign.com>
2020-09-10 12:31:15 +0100

    App.js updated to include home route

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ const projectZeroRouter = require('./routers/projectZero/router');
 const app = express()
 const port = process.env.PORT || 3001
 
+app.get('/', (req, res) => {
+    res.send('Welcome to my Portfolio API');
+})
+
 app.use('/project-zero', projectZeroRouter);
 
 app.listen(port, () => {
-  console.log(`API listening at http://localhost:${port}`)
+  console.log(`Server is running...`)
 })
